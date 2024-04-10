9f0b6e9907cad91fe8a9b9b4e515ce61d3d73deb
goisygor <ygorgoissenai@gmail.com>
2024-04-10 20:40:02 -0300

    10/04/24

---
+++ escola_completo/src/main/java/webapp/escola_completo/Repository/AdministradorRepository.java
@@ -0,0 +1,13 @@
+
+package webapp.escola_completo.Repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import webapp.escola_completo.Model.Administrador;
+import java.util.List;
+
+
+public interface AdministradorRepository extends CrudRepository<Administrador, String>{
+    Administrador findByCpf(String cpf);
+    Administrador findBySenha(String senha);
+}
